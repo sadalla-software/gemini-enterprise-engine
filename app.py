51717e59688802fbd723d6ff16ee38296b790d46
Your Name <you@example.com>
2026-06-13 22:16:44 +0200

    Fix: Supabase Invalid URL na kuboresha uimara wa kodi

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ if not st.session_state["logged_in"]:
         st.markdown(f'''
             <div class="header-logo-container">
                 <img src="data:image/png;base64,{logo_b64}" class="header-logo-img" />
-                <span class="header-logo-text">SADALLAH</span>
+                <span class="header-logo-text">Sadallah Software</span>
             </div>
         ''', unsafe_allow_html=True)
     else:
